198b461c79076817d038b17518fd678e9c4ba5ac
megha angadi <meghamangadi@gmail.com>
2021-08-26 16:33:59 +0530

    Added:Emp_AttendenceCheck

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,7 +4,14 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-
+       
+		int isFullTime = 1;
+    	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+    	if(empCheck == isFullTime)
+            System.out.println( "Employee is present");
+    	else
+    		System.out.println("Employee is absent");
 	}
+	
 
 }
