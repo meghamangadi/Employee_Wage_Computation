1b1b28dbf99bbc994de68f7214154d2401ee4309
megha angadi <meghamangadi@gmail.com>
2021-08-26 16:54:19 +0530

    Added:EmpDailyWageCalculation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,12 +5,18 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
        
-		int isFullTime = 1;
+		int Is_Full_Time = 1;
+		int EMP_RATE_PER_HOUR =20;
+		int empHrs = 0;
+		int empWage = 0;
     	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-    	if(empCheck == isFullTime)
-            System.out.println( "Employee is present");
+    	if(empCheck == Is_Full_Time)
+    		 empHrs=8;
     	else
-    		System.out.println("Employee is absent");
+    		empHrs=0;
+    	empWage =empHrs*EMP_RATE_PER_HOUR;
+    	System.out.println("Employee_wage:" + empWage );
+	
 	}
 	
 
