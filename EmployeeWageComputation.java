f3dcc850850f3e8ab2dc8ee7e73910ad6d1c735f
megha angadi <meghamangadi@gmail.com>
2021-08-26 19:39:18 +0530

    Added: EmpWage for 20 days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,25 +5,30 @@ public class EmployeeWageComputation {
 	public static final int Is_Part_Time = 1;
 	public static final int Is_Full_Time = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int Num_Of_Working_Days = 20;
 
 	public static void main(String[] args) {
 
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case Is_Part_Time:
-			empHrs = 4;
-			break;
-		case Is_Full_Time:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
+		int totalEmpWage = 0;
+		for (int day = 1; day <= Num_Of_Working_Days; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case Is_Part_Time:
+				empHrs = 4;
+				break;
+			case Is_Full_Time:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Employee_wage:" + empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee_wage:" + empWage);
-
+		System.out.println("Total Employee_wage:" + totalEmpWage);
 	}
 
 }
