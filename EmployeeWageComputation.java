c4929dd804393aa10efdc02c40fd7a8d6e906ca1
megha angadi <meghamangadi@gmail.com>
2021-08-26 19:01:11 +0530

    Added: EmpPartTimeWage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,18 +4,21 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-       
-		int Is_Full_Time = 1;
-		int EMP_RATE_PER_HOUR =20;
+		
+		int Is_Part_Time = 1;
+		int Is_Full_Time = 2;
+		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-    	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-    	if(empCheck == Is_Full_Time)
-    		 empHrs=8;
-    	else
-    		empHrs=0;
-    	empWage =empHrs*EMP_RATE_PER_HOUR;
-    	System.out.println("Employee_wage:" + empWage );
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		if (empCheck == Is_Part_Time)
+			empHrs = 4;
+		else if (empCheck == Is_Full_Time)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee_wage:" + empWage);
 	
 	}
 	
